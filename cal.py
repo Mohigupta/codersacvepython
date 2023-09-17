f2a5d44b7ed7b41c424c196d6084456a8e3c1062
Mohit Gupta <129882003+Mohigupta@users.noreply.github.com>
2023-09-17 18:01:45 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,23 @@
+number1 = int(input("enter the value of first no. ="))
+number2 = int(input("enter the value of second no. ="))
+add =number1 + number2
+print("the sum of ", number1, "and", number2, "is",add)
+
+sub =number1 - number2
+print("the subtractions of ", number1, "and", number2, "is",sub)
+
+div =number1 / number2
+print("the division of ", number1, "and", number2, "is",div)
+
+multi =number1 * number2
+print("the multiplication of ", number1, "and", number2, "is",multi)
+
+flr =number1 // number2
+print("the floor division of ", number1, "and", number2, "is",flr)
+
+exp =number1**number2
+print("the expontential division of ", number1, "and", number2, "is",flr)
+
+mud =number1   % number2
+print("the module  of ", number1, "and", number2, "is",mud)
+
